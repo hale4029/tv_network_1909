f9daa2a2ea17ec83bb5d5322cb19d35245cbeff6
Harrison Levin <harrison483@gmail.com>
2019-10-16 10:12:13 -0600

    network test created

---
+++ test/network_test.rb
@@ -0,0 +1,42 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+require './lib/network'
+
+class NetworkTest << Minitest::Test
+  def setup
+    @nbc = Network.new("NBC")
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+  end
+
+  def test_it_exists
+    assert_instance_of Network, @nbc
+  end
+
+  def test_network_name
+    assert_equal "Parks and Recreation", @nbc.name
+  end
+
+  def test_empty_shows_array
+    assert_equal [], @nbc.shows
+  end
+
+  def test_ability_to_add_shows_to_network
+    @nbc.add_show(@knight_rider)
+    assert_equal [@knight_rider], @nbc.shows
+
+    @nbc.add_show(@parks_and_rec)
+    assert_equal [@michael_knight, @parks_and_rec], @nbc.shows
+  end
+
+  def test_highest_paid_actor
+    assert_equal "David Hasselhoff", @nbc.highest_paid_actor
+  end
+  
+end
